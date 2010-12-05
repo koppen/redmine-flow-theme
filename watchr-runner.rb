782b2ef38e11aa014fadc446b2b62cdbcd9b88c4
Jakob Skjerning <jakob@mentalized.net>
2010-12-05 15:33:57 +0100

    Initial setup

---
+++ watchr-runner.rb
@@ -0,0 +1,34 @@
+if __FILE__ == $0
+  puts "Run with: watchr #{__FILE__}. \n\nRequired gems: watchr rev"
+  exit 1
+end
+
+# --------------------------------------------------
+# Convenience Methods
+# --------------------------------------------------
+def run(cmd)
+  puts(cmd)
+  system(cmd)
+end
+
+def rebuild_stylesheets
+  run "sass sass/application.sass stylesheets/application.css"
+end
+
+# --------------------------------------------------
+# Watchr Rules
+# --------------------------------------------------
+watch( '^sass/(.*)' ) { |m| rebuild_stylesheets }
+
+# --------------------------------------------------
+# Signal Handling
+# --------------------------------------------------
+# Ctrl-\
+Signal.trap('QUIT') do
+  rebuild_stylesheets
+end
+ 
+# Ctrl-C
+Signal.trap('INT') { abort("\n") }
+
+puts "Watching.."
