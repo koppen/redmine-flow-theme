413760d81123f086795aa905acd9f0605c0050dc
Jakob Skjerning <jakob@mentalized.net>
2011-03-24 11:00:36 +0100

    Bring in icon declarations from the default Redmine stylesheet

--- watchr-runner.rb
+++ watchr-runner.rb
@@ -19,6 +19,7 @@ end
 # Watchr Rules
 # --------------------------------------------------
 watch( '^sass/(.*)' ) { |m| rebuild_stylesheets }
+watch( '^sass/default/(.*)' ) { |m| rebuild_stylesheets }
 watch( '^sass/mixins/(.*)' ) { |m| rebuild_stylesheets }
 
 # --------------------------------------------------
